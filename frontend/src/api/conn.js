321ec010f2109d0611d00c73e9f7553083cf46be
RobertRedmiles <132864353+RobertRedmiles@users.noreply.github.com>
2023-10-03 22:28:21 -0600

    Update conn.js

--- frontend/src/api/conn.js
+++ frontend/src/api/conn.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://w3-ticketing.onrender.com/";
+const BASE_URL = "https://w3-ticketing.onrender.com";
 const apiConn = axios.create({baseURL: BASE_URL});
 
 export default apiConn;
